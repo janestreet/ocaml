d3d88c2e30f36a59cce8672db130c0e7b6bf5ab6
Leo White <lpw25@cl.cam.ac.uk>
2016-07-22 10:36:26 +0100

    Fixes for GC credit handling

--- byterun/major_gc.c
+++ byterun/major_gc.c
@@ -730,6 +730,8 @@ void caml_major_collection_slice (intnat howmuch)
                / caml_stat_heap_wsz / caml_percent_free / 2.0;
     }
     caml_major_work_credit += filt_p;
+    /* Limit work credit to 1.0 in p units */
+    caml_major_work_credit = fmin(caml_major_work_credit, 1.0);
   }
 
   p = filt_p;
