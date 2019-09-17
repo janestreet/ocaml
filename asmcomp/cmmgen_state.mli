9206626d42628d753bdd84af1994c1104c4defeb
Alain Frisch <alain.frisch@lexifi.com>
2019-09-17 10:22:08 +0200

    Decide unboxing of let-bound expressions based on their Cmm translation (#2165)

--- asmcomp/cmmgen_state.mli
+++ asmcomp/cmmgen_state.mli
@@ -40,3 +40,7 @@ val data_items : unit -> Cmm.data_item list
 val next_function : unit -> Clambda.ufunction option
 
 val no_more_functions : unit -> bool
+
+val set_structured_constants : Clambda.preallocated_constant list -> unit
+
+val get_structured_constant : string -> Clambda.ustructured_constant option
